0c90897ab90473df1ee9c34f73e1f9630df35483
searcheuchheang <searcheuchheang@gmail.com>
2022-04-03 16:25:09 +0700

    fetch api of recipe

---
+++ src/components/Popular.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+
+const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+  };
+  return (
+    <div>
+      {popular.map((recipe) => (
+        <div key={popular.id}>
+          <p >{recipe.title}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Popular;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Popular from "../components/Popular";
+import Veggie from "../components/Veggie";
+
+const Home = () => (
+  <>
+    <h1>Hello</h1>
+    <div>
+      <Popular />
+    </div>
+    <div>
+      <Veggie />
+    </div>
+  </>
+);
+
+export default Home;
